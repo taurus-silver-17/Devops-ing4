03f1cba29e0e285c5e2aeab7316797c636cb6d74
Lilian Delaplace <lilian.delaplace@gmail.com>
2019-12-23 14:49:51 +0100

    info code

--- models.py
+++ models.py
@@ -1,4 +1,11 @@
-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# This web application is designed to run on a Raspberry Pi.
+# GPIOs are used to operate module relays then lights and stuff.
+# Ver 2.0
+# License GNU GPLv3 <https://www.gnu.org/licenses/gpl.html>
+####
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
